e4b8211a0ef9381cda73128bc1cef0d380a08022
shiryu1202 <106821745+shiryu1202@users.noreply.github.com>
2023-12-15 18:09:25 +0900

    建築時にオブジェクトが重なって建築できないように修正

---
+++ Assets/Scripts/Building/ObstacleDetection.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDetection : MonoBehaviour
+{
+    // 衝突が発生しているオブジェクトの数
+    private int colliderCount = 0;
+
+    // 衝突が始まったときに呼ばれるメソッド
+    private void OnTriggerEnter(Collider collision)
+    {
+        // 衝突が発生したらカウントを増やす
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Block"))
+            colliderCount++;
+    }
+
+    // 衝突が終わったときに呼ばれるメソッド
+    private void OnTriggerExit(Collider collision)
+    {
+        // 衝突が終わったらカウントを減らす
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Block"))
+            colliderCount--;
+    }
+
+    public bool IsHit()
+    {
+        Debug.Log(colliderCount);
+        return colliderCount > 0;
+    }
+}
